4b4b65aefef629d1f5f6104c07d55f69a0cb7899
smeenguyen <nguyenvanquanghuy99@gmail.com>
2022-11-20 10:55:38 +0700

    Save

--- src/main/java/com/mongodb/Save.java
+++ src/main/java/com/mongodb/Save.java
@@ -11,23 +11,23 @@ public class Save {
     }
 
     public static void recentWords(Search recent, String searchString) {
-        JSONObject jsonRecentWords = new JSONObject();
-        jsonRecentWords.put(recent.getCurentWord(), searchString);
+        JSONObject jsonRecentWord = new JSONObject();
+        jsonRecentWord.put(recent.getCurentWord(), searchString);
         List<String> recentWordsList = new ArrayList<>(Arrays.asList(jsonRecentWordsList));
-        recentWordsList.add(jsonRecentWords.toJSONString());
+        recentWordsList.add(jsonRecentWord.toJSONString());
         Set<String> setArr = new HashSet<>(recentWordsList);
         jsonRecentWordsList = setArr.toArray(new String[0]);
         try {
             String filePath = new File("").getAbsolutePath();
             filePath.concat("/src/main/save/recentWords.json");
             System.out.println(filePath);
-            FileWriter file = new FileWriter(filePath);
+            FileWriter file = new FileWriter("D:/recentWords.json");
             file.write(Arrays.toString(jsonRecentWordsList));
             file.close();
         } catch (IOException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
-        System.out.println("JSON file created: "+jsonRecentWords);
+        System.out.println("JSON file created: "+jsonRecentWord);
     }
 }
